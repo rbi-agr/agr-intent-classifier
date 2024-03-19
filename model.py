a368194d5a4566e4fb73ab5fe51eb14ceb2f805e
EsMagicoKunal <kunal@esmagico.in>
2024-03-20 01:05:16 +0530

    added model and api endpoint

---
+++ model.py
@@ -0,0 +1,24 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+from request import ModelRequest
+
+class Model():
+    def __new__(cls, context):
+        cls.context = context
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Model, cls).__new__(cls)
+        model = 'KunalEsM/bank_complaint_intent_classifier'
+        cls.tokenizer = AutoTokenizer.from_pretrained(model)
+        cls.model = AutoModelForSequenceClassification.from_pretrained(model)
+        cls.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        cls.model.to(cls.device)
+        return cls.instance
+
+
+    async def inference(self,  request: ModelRequest):
+        inputs = self.tokenizer(request.text, return_tensors="pt")
+        inputs = {key: value.to(self.device) for key, value in inputs.items()}
+        with torch.no_grad():
+            predicted_label = self.model(**inputs).logits.argmax().item()
+        
+        return predicted_label
