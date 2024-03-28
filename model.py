e4f7a8b113a65e04732b5439a117fa2e50afcf1a
EsMagicoKunal <kunal@esmagico.in>
2024-03-28 12:20:30 +0530

    changed model

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class Model():
         cls.context = context
         if not hasattr(cls, 'instance'):
             cls.instance = super(Model, cls).__new__(cls)
-        model = 'KunalEsM/bank_complaint_intent_classifier'
+        model = 'KunalEsM/bank_complaint_intent_classifier_v2'
         cls.tokenizer = AutoTokenizer.from_pretrained(model)
         cls.model = AutoModelForSequenceClassification.from_pretrained(model)
         cls.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
