9372ab3b3fdb24e1ff4de16b369b0abd77b34b02
EsMagicoKunal <kunal@esmagico.in>
2024-05-20 16:50:38 +0530

    changes

--- model.py
+++ model.py
@@ -2,31 +2,25 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 from request import ModelRequest
 import torch.nn.functional as Fnc
-
 class Model():
     def __new__(cls, context):
         cls.context = context
         if not hasattr(cls, 'instance'):
             cls.instance = super(Model, cls).__new__(cls)
-        model = 'KunalEsM/bank_complaint_intent_classifier_v2'
-        cls.tokenizer = AutoTokenizer.from_pretrained(model)
-        cls.model = AutoModelForSequenceClassification.from_pretrained(model)
+        cls.tokenizer = AutoTokenizer.from_pretrained("./saved_tokenizers")
+        cls.model = AutoModelForSequenceClassification.from_pretrained("./saved_models")
         cls.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         cls.model.to(cls.device)
         cls.threshold = 0.5
         return cls.instance
-
-
     async def inference(self,  request: ModelRequest):
         inputs = self.tokenizer(request.text, return_tensors="pt")
         inputs = {key: value.to(self.device) for key, value in inputs.items()}
         with torch.no_grad():
             logits = self.model(**inputs).logits
             probabilities = Fnc.softmax(logits, dim=1)
-
             print("All logits:", logits)
             print("Probabilities:", probabilities)
-
         if probabilities.max() < self.threshold:
             return 11
         else:
